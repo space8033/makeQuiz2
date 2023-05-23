1e16ace224b2501e8fefa1a2b357d6826757ec79
space8033 <space8237@naver.com>
2023-05-23 18:05:53 +0900

    init

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Member member1 = new Member("오우주", "011234567899");
+        Member member2 = new Member("성유진", "01023456789");
+        Member member3 = new Member("유승주", "0112345678");
+
+        System.out.println(member1);
+        System.out.println(member2);
+        System.out.println(member3);
+
+    }
+}
---
+++ src/Member.java
@@ -0,0 +1,51 @@
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Member {
+    private String name;
+    private String phoneNumber;
+    private String registeredAt;
+
+    public Member(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = makePhoneNumber(phoneNumber);
+        this.registeredAt = makeTimeStamp();
+    }
+
+    private String makePhoneNumber(String phoneNumber) {
+        //핸드폰 번호 정규 표현식 : ^01(?:0|1|[6-9]) - (?:\d{3}|\d{4}) - \d{4}$
+        if(phoneNumber.length() == 11) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(phoneNumber.substring(0, 3));
+            sb.append("-");
+            sb.append(phoneNumber.substring(3,7));
+            sb.append("-");
+            sb.append(phoneNumber.substring(7));
+
+            return sb.toString();
+
+        }else if(phoneNumber.length() == 10){
+            StringBuilder sb = new StringBuilder();
+            sb.append(phoneNumber.substring(0, 3));
+            sb.append("-");
+            sb.append(phoneNumber.substring(3,6));
+            sb.append("-");
+            sb.append(phoneNumber.substring(6));
+
+            return sb.toString();
+        }else {
+            return "잘못된 전화번호 입니다";
+        }
+    }
+
+    private String makeTimeStamp() {
+        Date now = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 a HH:mm:ss");
+
+        return sdf.format(now);
+    }
+}
